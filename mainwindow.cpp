bc1c8448717d0e16e8b316006c5615003fc05736
guilbertb <guilbert@workboot.fr>
2019-06-03 16:32:54 +0200

    second commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,14 +3,19 @@
 #include <QSerialPort>
 #include <QDebug>
 
+
+    QString Str="G28\n"; // Message à envoyer
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
+    ui->lineEdit->setText(Str);
+
     myserie = new QSerialPort(this);
-    myserie->setPortName(QLatin1String("/dev/ttyACM0"));
+    myserie->setPortName(QLatin1String("/dev/ttyUSB0"));
     // Ouverture en écriture et lecture
     if(myserie->open(QIODevice::ReadWrite) == true)
     {
@@ -22,15 +27,17 @@ MainWindow::MainWindow(QWidget *parent) :
     return;
     }
     // Configuration du port
-    myserie->setBaudRate(QSerialPort::Baud9600,QSerialPort::AllDirections);
+    myserie->setBaudRate(250000,QSerialPort::AllDirections);
     myserie->setDataBits(QSerialPort::Data8);
     myserie->setFlowControl(QSerialPort::NoFlowControl);
     myserie->setParity(QSerialPort::NoParity);
     myserie->setStopBits(QSerialPort::OneStop);
-    myserie->waitForBytesWritten(100);
-    myserie->waitForReadyRead(100);
+    myserie->waitForBytesWritten(50);
+    myserie->waitForReadyRead(50);
     // Connexion du signal readyRead au slot lecture_serie()
     connect(myserie,SIGNAL(readyRead()),this,SLOT(lecture_serie()));
+
+
 }
 
 MainWindow::~MainWindow()
@@ -44,8 +51,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString Str="Bonjour"; // Message à envoyer
-    int Nb=myserie->write(Str.toLocal8Bit(),Str.length());
+    Str=ui->lineEdit->text()+"\n";
+
+    //int Nb=
+    myserie->write(Str.toLocal8Bit(),Str.length());
+
+    qDebug() << Str;
     // La méthode toAscii() de la classe QString Qt4.8 est remplacée
     // par toLocal8Bit()de la classe QString Qt5.2
 
@@ -54,12 +65,14 @@ void MainWindow::on_pushButton_clicked()
 
 void MainWindow::lecture_serie ()
 {
-char Data[10];
+char Data[200];
 for (int i=0;i<sizeof(Data);i++)Data[i]=0;
 int Nb= myserie->readLine(Data,sizeof(Data));
 //int Nb= myserie->read(Data,sizeof(Data));
 if(Nb)
 { // Des données ont été reçues
+    ui->textBrowser->append(Data);
+
 qDebug() << Data ;
 }
 }
---
+++ ui_mainwindow.h
@@ -0,0 +1,86 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.5
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QTextBrowser *textBrowser;
+    QLineEdit *lineEdit;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(400, 300);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(270, 20, 89, 25));
+        textBrowser = new QTextBrowser(centralWidget);
+        textBrowser->setObjectName(QStringLiteral("textBrowser"));
+        textBrowser->setGeometry(QRect(10, 50, 381, 181));
+        lineEdit = new QLineEdit(centralWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(10, 20, 251, 25));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 22));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Gcode", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "&Envoyer", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
